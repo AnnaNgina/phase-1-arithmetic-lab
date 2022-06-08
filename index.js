8edbb5056ff66db232668779eb4d7ebbf1021cdd
annastacia <annastaciangina96@gmail.com>
2022-06-08 10:18:09 +0300

    commit tree ffb2002bb5619ef97a8da3c11daecaccf2d74df6

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+let num1 =31;
+ let num2= 2;
+const multiply =(num1*num2);
+
+
+
+let random = Math.floor(Math.random()*10)+1
+console.log(random)
+
+let num3=9;
+let num4=5;
+const mod =num3%num4;
+
+console.log(mod )
+
+ let max=20;
+ Math.max[15,16,17,18,19,20]
+ console.log(max)
